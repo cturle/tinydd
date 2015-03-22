2bb19d7f05d9ed88e3a69e85f73f673bc41d7d40
Christophe Turle <cturle@yahoo.com>
2015-03-22 13:08:25 +0100

    + remove-race

--- src/tinydd/metamodel.clj
+++ src/tinydd/metamodel.clj
@@ -1,22 +1,15 @@
-(ns tinydd.metamodel)
+(ns tinydd.metamodel
+  (:require [utils.clojure :refer :all]) )
 
-
-(defn throw-if-not
-  "return a function that return FN applied to args if not nil, else throw an Exception with message MSG"
-  [FN MSG]
-  (fn [& args]
-    (let [R (apply FN args)]
-      (if (nil? R) (throw (Exception. MSG)) R) )))
-
-
+; todo : move to utils.model
 (defn new-model
   "create and return a new model"
   []
   {:next-id 1}
   )
 
-
-(declare get-all-race, add-race, the-race-name)
+; todo : + remove-race
+(declare get-all-race, add-race, remove-race, the-race-name)
 
 
 (defn get-all-race
@@ -25,19 +18,26 @@
   (get M :all-race []) )
 
 (defn add-race
-  "return an updated model from M where a new Race has been added with name N"
-  [M N]
+  "[M1 N] -> [M2 R].
+  return an updated model M2 from M1 where a new Race R has been added with name N"
+  [M1 N]
   (let [GET1          (throw-if-not get "model wo :next-id")
-        NEXT-ID       (GET1 M :next-id)
-        NEW-RACE-ID   NEXT-ID
+        NEXT-ID       (GET1 M1 :next-id)
+        R             NEXT-ID
         NEW-NEXT-ID   (inc NEXT-ID)
-        NEW-ALL-RACE  (conj (get M :all-race []) NEW-RACE-ID)
-        NEW-RACE      {:id NEW-RACE-ID, :name N}
-        ]
-    [(assoc M :next-id     NEW-NEXT-ID,
-              :all-race    NEW-ALL-RACE,
-              NEW-RACE-ID  NEW-RACE )
-     NEW-RACE-ID ]))
+        NEW-ALL-RACE  (conj (get M1 :all-race []) R)
+        NEW-RACE      {:id R, :name N}
+        M2            (assoc M1 :next-id NEW-NEXT-ID, :all-race NEW-ALL-RACE, R NEW-RACE) ]
+    [M2, R] ))
+
+(defn remove-race
+  "[M1 R] -> M2
+  return an updated model M2 from M1 where Race R has been removed"
+  [M1 R]
+  (let [NEW-ALL-RACE  (remove #{R} (get M1 :all-race []))
+        M2            (dissoc M1 R)
+        M2            (assoc M2 :all-race NEW-ALL-RACE) ]
+  M2 ))
 
 (defn the-race-name
   "return the name of the race R in the model M"
--- test/tinydd/metamodel_test.clj
+++ test/tinydd/metamodel_test.clj
@@ -4,13 +4,15 @@
 
 (deftest race-test
   (testing "Race API"
-    (let [M0 (new-model)]
-      (is (= [] (get-all-race M0)))
-      (let [ N      "race-1"
-            [M1 R1] (add-race M0 N) ]
-        (is (= [R1] (get-all-race M1)))
-        (is (= N (the-race-name M1 R1)))
-      ))))
+    (let [M0      (new-model)
+          N       "race-1"
+          [M1 R1] (add-race M0 N)
+          M2      (remove-race M1 R1) ]
+      (is (= []   (get-all-race M0)))
+      (is (= [R1] (get-all-race M1)))
+      (is (= N    (the-race-name M1 R1)))
+      (is (= []   (get-all-race M2)))
+      )))
 
 ; (run-tests)
 
